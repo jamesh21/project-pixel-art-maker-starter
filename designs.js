438ad747c7c0a231060bbed960e27658279c5c18
jamesh21 <jamesh21@uw.edu>
2018-04-30 12:40:56 -0700

    Add Color Picker event listener

--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
 // Select color input
+const colorPicker = document.querySelector('#colorPicker');
+let currentColor = colorPicker.value;
+colorPicker.addEventListener('change', function (event) {
+  currentColor = event.target.value;
+});
+
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  return null;
 }
