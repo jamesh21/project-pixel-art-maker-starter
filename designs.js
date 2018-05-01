fdc502d489561c87c7cb6bad81bc8b7a5283594e
jamesh21 <jamesh21@uw.edu>
2018-05-01 01:35:46 -0700

    Add grid with color

--- designs.js
+++ designs.js
@@ -5,11 +5,32 @@ colorPicker.addEventListener('change', function (event) {
   currentColor = event.target.value;
 });
 
-
 // Select size input
+const sizePicker = document.querySelector('#sizePicker');
+sizePicker.addEventListener('submit', function (event) {
+  event.preventDefault();
+  let currentWidth = document.getElementById('inputWidth').value;
+  let currentHeight = document.getElementById('inputHeight').value;
+  console.log(currentWidth);
+  makeGrid(currentWidth, currentHeight);
+});
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-  return null;
+function makeGrid(columnNum, rowNum) {
+  const canvas = document.querySelector('#pixelCanvas');
+  canvas.addEventListener('click', paintSquare);
+  for (let i = 0; i < rowNum; i++) {
+    const newRow = document.createElement('tr');
+    for (let j = 0; j < columnNum; j++) {
+      const newCol = document.createElement('td');
+      newRow.appendChild(newCol);
+    }
+
+    canvas.appendChild(newRow);
+  }
+}
+
+function paintSquare(event) {
+  event.target.style.backgroundColor = currentColor;
 }
