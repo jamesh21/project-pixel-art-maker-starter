9131d32a0ff2070ef9eb728aa15df4842de0c507
jamesh21 <jamesh21@uw.edu>
2018-05-03 01:55:26 -0700

    Add draw functionality to the grid

--- designs.js
+++ designs.js
@@ -1,25 +1,31 @@
-// Select color input
+// Selectors
 const colorPicker = document.querySelector('#colorPicker');
+const sizePicker = document.querySelector('#sizePicker');
+const canvas = document.querySelector('#pixelCanvas');
+
+// current color that is chosen
 let currentColor = colorPicker.value;
+
+// Add Listeners
+canvas.addEventListener('click', paintSquare);
+
 colorPicker.addEventListener('change', function (event) {
   currentColor = event.target.value;
 });
 
-// Select size input
-const sizePicker = document.querySelector('#sizePicker');
 sizePicker.addEventListener('submit', function (event) {
   event.preventDefault();
+  if (canvas.hasChildNodes()) {
+    resetGrid();
+  }
+
   let currentWidth = document.getElementById('inputWidth').value;
   let currentHeight = document.getElementById('inputHeight').value;
-  console.log(currentWidth);
   makeGrid(currentWidth, currentHeight);
 });
 
-// When size is submitted by the user, call makeGrid()
-
+// Dynamically creates the canvas grid
 function makeGrid(columnNum, rowNum) {
-  const canvas = document.querySelector('#pixelCanvas');
-  canvas.addEventListener('click', paintSquare);
   for (let i = 0; i < rowNum; i++) {
     const newRow = document.createElement('tr');
     for (let j = 0; j < columnNum; j++) {
@@ -31,6 +37,17 @@ function makeGrid(columnNum, rowNum) {
   }
 }
 
+// Paints the specified square with the current color
 function paintSquare(event) {
-  event.target.style.backgroundColor = currentColor;
+  if (event.target.nodeName === 'TD') {
+    event.target.style.backgroundColor = currentColor;
+  }
+
+}
+
+// function to reset the canvas grid
+function resetGrid() {
+  while (canvas.hasChildNodes()) {
+    canvas.removeChild(canvas.lastChild);
+  }
 }
